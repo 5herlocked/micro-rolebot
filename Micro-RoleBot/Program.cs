06c849c43a1e5a69348266551b8a1c8ecaf3519a
Shardul Vaidya <cam.v737@gmail.com>
2021-05-04 00:56:23 -0400

    Initial application setup

---
+++ Micro-RoleBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Micro_RoleBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
